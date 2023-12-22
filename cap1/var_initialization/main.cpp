c1c9884a4244125ac4d31af715dd23b02158b6b9
Enrico Coppolecchia <enricocoppolecchia@outlook.com>
2023-12-22 16:39:48 +0100

    init

---
+++ cap1/var_initialization/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    double d1 = 2.3;
+    double d2{2.3};    // evita le conversioni con perdita di informazione
+    double d3 = {2.3}; // il simbolo = e' facoltativo
+
+    auto b = true;
+    auto ch = 'x';
+    auto i = 123;
+    auto d = 1.2;
+    //auto z = sqrt(y);
+
+
+
+    return 0;
+}
